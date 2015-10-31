126aa569631ef399f19aeae3571aeab99df9765c
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 16:55:48 +0800

    update reademe

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# chineseregion
+# 中国地区库
 a chinese region lib for python
--- src/chineseregion/__init__.py
+++ src/chineseregion/__init__.py
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
 __author__ = 'xuanwo'
 
-def main():
+import json
+
+
+def find(str):
     pass
+
+
+def main():
+    with open('region.json') as fp:
+        data = json.load(fp)
+        print(list(data.values()))
+
 
 if __name__ == '__main__':
     main()
