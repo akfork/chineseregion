e82b22f06b7c30ab635c7d7122251eb007daace4
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 21:01:54 +0800

    add package_data option

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ from setuptools import setup, find_packages
 setup(
     name="chineseregion",
     version="0.0.1",
-    description = "a chinese region lib for python",
-    author = "xuanwo",
-    author_email = "xuanwo.cn@gmail.com",
+    description="a chinese region lib for python",
+    author="xuanwo",
+    author_email="xuanwo.cn@gmail.com",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    include_package_data = True
+    package_data={'': '*.json'},
+    include_package_data=True
 )
