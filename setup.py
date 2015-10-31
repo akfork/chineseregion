08699939c2776ef4e256086554a672c0ea9c34a2
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 20:21:35 +0800

    try to pack

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'xuanwo'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "chineseregion",
+    version = "0.0.1",
+    packages = find_packages(),
+)
