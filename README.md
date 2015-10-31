f2044ace722e4e39fe31a65fa0b54973f481c4be
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 19:44:24 +0800

    add find function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 中国地区库
 a chinese region lib for python
+
+# 支持功能
+给定一个地址，找出最小行政单位并给出行政区划代码
--- src/chineseregion/__init__.py
+++ src/chineseregion/__init__.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 __author__ = 'xuanwo'
 
+import os, sys
 import json
 
 
-def find(str):
-    pass
+def find(str, data):
+    city = []
+    for i in sorted(data.keys()):
+        if str.find(data[i]) != -1:
+            city.append(data[i])
+    return city
 
 
-def main():
+def main(argv):
     with open('region.json') as fp:
         data = json.load(fp)
-        print(list(data.values()))
+    for i in range(0, len(argv)):
+        answer = find(argv[i], data)
+        if(len(answer)==0):
+            print("Not Found")
+        else:
+            print(answer)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
