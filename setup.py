3cfe80271d8802dcd1e979f5da70e5770e66f446
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 20:38:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ __author__ = 'xuanwo'
 from setuptools import setup, find_packages
 
 setup(
-    name = "chineseregion",
-    version = "0.0.1",
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    package_data = {
-        '': ['*.json'],
-    }
+    name="chineseregion",
+    version="0.0.1",
+    description = "a chinese region lib for python",
+    author = "xuanwo",
+    author_email = "xuanwo.cn@gmail.com",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data = True
 )
