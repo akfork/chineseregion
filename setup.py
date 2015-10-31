dd35f478b0d53fb525317ad5176da0121c63fa6f
Xuanwo <xuanwo.cn@gmail.com>
2015-10-31 20:27:24 +0800

    add package

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ from setuptools import setup, find_packages
 setup(
     name = "chineseregion",
     version = "0.0.1",
-    packages = find_packages(),
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    package_data = {
+        '': ['*.json'],
+    }
 )
