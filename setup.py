d7138d92a45817b77986f948401bfb72b6caf0a0
Xuanwo <xuanwo.cn@gmail.com>
2015-11-01 15:25:19 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,31 @@ entry_points = {
     ]
 }
 
+with open("requirements.txt") as f:
+    requires = [l for l in f.read().splitlines() if l]
+
 setup(
     name="chineseregion",
     version= chineseregion.__version__,
     description="a chinese region lib for python",
     author="xuanwo",
     author_email="xuanwo.cn@gmail.com",
+    keywords="chinese, region, lib",
+    license="MIT License",
     packages=find_packages(),
     entry_points=entry_points,
-    include_package_data=True
+    install_requires=requires,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha ',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
