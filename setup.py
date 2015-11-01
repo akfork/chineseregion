383c15782c3afce766fe2ba1033a79c1a2e3f914
Xuanwo <xuanwo.cn@gmail.com>
2015-11-01 14:41:34 +0800

    finally finished 0.0.1

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@
 __author__ = 'xuanwo'
 
 from setuptools import setup, find_packages
+import chineseregion
+
+entry_points = {
+    "console_scripts": [
+        "chineseregion = chineseregion.main:main",
+    ]
+}
 
 setup(
     name="chineseregion",
-    version="0.0.1",
+    version= chineseregion.__version__,
     description="a chinese region lib for python",
     author="xuanwo",
     author_email="xuanwo.cn@gmail.com",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    package_data={'': ['*.json']},
+    packages=find_packages(),
+    entry_points=entry_points,
     include_package_data=True
 )
